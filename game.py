41b098054b7a640ada7648b5395843bceeeab63a
PakoMtzR <francisco.2k1@gmail.com>
2024-08-02 15:38:32 -0600

    sonidos de disparos, barra de vida y menu principal

--- game.py
+++ game.py
@@ -17,16 +17,24 @@ clock = pygame.time.Clock()
 
 # Cargamos sonidos del juego
 war_sound = pygame.mixer.Sound("sounds/war_sound.mp3")
+shoot_sound = pygame.mixer.Sound("sounds/shoot_sound.mp3")
 
 # Creamos y configuramos la pantalla del juego
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption(TITLE)
 pygame.display.set_icon(ICON)
 
+# Creamos las fuentes para las letras del juego
+font = pygame.font.SysFont("Consolas", 20)
+title_font = pygame.font.SysFont("Consolas", 30)
+label_title = title_font.render("Press any key to begin", True, (255,255,255))
+label_lost = title_font.render("You Lost!", True, (255,255,255))
+
 class Ship:
     def __init__(self, pos_x, pos_y, health = 100):
         self.x = pos_x
         self.y = pos_y
+        self.max_health = health
         self.health = health
         self.speed = 3
         self.size = 50
@@ -36,14 +44,18 @@ class Ship:
         self.laser_color = None
         self.lasers_goes_down = False
     
+    # Dibuja la nave en la pantalla y si tiene lasers los dibuja tambien
     def draw(self, screen):
         screen.blit(self.img, (self.x, self.y))
         for laser in self.lasers:
             laser.draw()
     
+    # Al disparar crea un objeto Laser y lo agrega en la lista de laseres
     def shoot(self):
-        laser = Laser(self.x + self.size//2, self.y+10, self.laser_color, self.lasers_goes_down)
-        self.lasers.append(laser)
+        if self.y >= 0:
+            laser = Laser(self.x + self.size//2, self.y+10, self.laser_color, self.lasers_goes_down)
+            self.lasers.append(laser)
+            shoot_sound.play()
     
     def move_lasers(self, obj):
         for laser in self.lasers:
@@ -51,8 +63,9 @@ class Ship:
             if laser.off_screen():
                 self.lasers.remove(laser)
             elif laser.collision(obj):
-                obj.health -= 10
                 self.lasers.remove(laser)
+                if obj.health > 0:
+                    obj.health -= 10
 
 class Player(Ship):
     def __init__(self, pos_x, pos_y, health = 100):
@@ -63,6 +76,14 @@ class Player(Ship):
         self.laser_color = (255, 0, 0)
         self.lives = 5
         self.destroyed_enemies = 0
+    
+    def healthbar(self, screen):
+        pygame.draw.rect(screen, (255,0,0), (self.x, self.y + self.size + 2, self.size, 5))
+        pygame.draw.rect(screen, (0,255,0), (self.x, self.y + self.size + 2, self.size*(self.health/self.max_health), 5))
+
+    def draw(self, screen):
+        super().draw(screen)
+        self.healthbar(screen)
     
     def move_lasers(self, objs):
         for laser in self.lasers:
@@ -116,106 +137,117 @@ class Laser():
     def collision(self, obj):
         return collide(self, obj)
 
+# Funciones para verificar colisiones
 def collide(obj1, obj2):
     offset_x = obj2.x - obj1.x
     offset_y = obj2.y - obj1.y
     return obj1.mask.overlap(obj2.mask, (offset_x, offset_y)) != None
 
-# Variables del juego
-font = pygame.font.SysFont("Consolas", 20)
-lost_font = pygame.font.SysFont("Consolas", 50)
-level = 0
-player = Player(WIDTH//2, HEIGHT//2)
-enemies = []
-wave_length = 0
-lost = False
-lost_count = 0
+# Juego
+def run_game():
+    # Variables del juego
+    level = 0
+    player = Player(WIDTH//2, HEIGHT//2)
+    enemies = []
+    wave_length = 0
+    lost = False
+    lost_count = 0
 
-def redraw_window():
-    # Rellenamos el fondo
-    screen.fill(BACKGROUND)     
+    def redraw_window():
+        screen.fill(BACKGROUND)     # Rellenamos el fondo    
 
-    # Escribimos los labels de nivel, vidas, etc.
-    label_level = font.render(f"Level: {level}", True, (255,255,255))
-    label_lives = font.render(f"Lives: {player.lives}", True, (255,255,255))
-    label_health = font.render(f"Health: {player.health}", True, (255,255,255))
-    label_destroyed = font.render(f"Destroyed: {player.destroyed_enemies}", True, (255,255,255))
-    screen.blit(label_level, (10,10))
-    screen.blit(label_lives, (10,30))
-    screen.blit(label_health, (10,50))
-    screen.blit(label_destroyed, (10,70))
+        # Escribimos los labels de nivel, vidas, etc.
+        label_level = font.render(f"Level: {level}", True, (255,255,255))
+        label_lives = font.render(f"Lives: {player.lives}", True, (255,255,255))
+        label_destroyed = font.render(f"Destroyed: {player.destroyed_enemies}", True, (255,255,255))
+        screen.blit(label_level, (10,10))
+        screen.blit(label_lives, (10,30))
+        screen.blit(label_destroyed, (10,50))
 
-    # Si el jugador pierde mostramos el mensaje de derrota
-    if lost:
-        label_lost = lost_font.render("You Lost!", True, (255,255,255))
-        screen.blit(label_lost, ((WIDTH//2)-(label_lost.get_width()//2), (HEIGHT//2)-(label_lost.get_height()//2)))
+        # Si el jugador pierde mostramos el mensaje de derrota
+        if lost:
+            screen.blit(label_lost, ((WIDTH//2)-(label_lost.get_width()//2), (HEIGHT//2)-(label_lost.get_height()//2)))
 
-    player.draw(screen)
-    player.move_lasers(enemies)
-    
-    # Movemos a los enemigos y los eliminamos si salen de la pantalla
-    for enemy in enemies:
-        enemy.move(screen)
-        enemy.move_lasers(player)
+        # Dibuja al jugador y sus lasers
+        player.move_lasers(enemies)
+        player.draw(screen)
+        
+        # Movemos a los enemigos y los eliminamos si salen de la pantalla
+        for enemy in enemies:
+            enemy.move_lasers(player)
+            enemy.move(screen)
 
-        if randrange(0, 1*60) == 1:
-            enemy.shoot()
+            if randrange(0, 1*60) == 1:
+                enemy.shoot()
 
-        if collide(enemy, player):
-            player.health -= 10
-            player.destroyed_enemies += 1
-            enemies.remove(enemy)
-        elif enemy.y > HEIGHT:
-            player.lives -= 1
-            enemies.remove(enemy)
+            if collide(enemy, player):
+                player.health -= 10
+                player.destroyed_enemies += 1
+                enemies.remove(enemy)
+            elif enemy.y > HEIGHT:
+                enemies.remove(enemy)
+                if player.lives > 0:
+                    player.lives -= 1
 
-    # Actualizamos la pantalla
+        # Actualizamos la pantalla
+        pygame.display.update()
+
+
+    run = True
+    while run:
+        clock.tick(FPS)
+        redraw_window()
+
+        # Si pierdes todas las vidas o destruyen la nave del jugador entonces pierde el juego
+        if player.lives <= 0 or player.health <= 0:
+            lost = True
+            lost_count += 1
+            if lost_count > FPS*3:
+                run = False
+            else:
+                continue
+
+        # Si eliminan todos los enemigos, sube de nivel y agregamos nuevos enemigos
+        if not enemies:
+            level += 1
+            wave_length += 5
+            war_sound.play()
+            for i in range(wave_length):
+                enemy = Enemy(randrange(0, WIDTH-SHIP_SIZE), randrange(-1500, -100))
+                enemies.append(enemy)
+
+        # Deteccion de eventos
+        for event in pygame.event.get():
+            # Cerrar la ventana del juego
+            if event.type == pygame.QUIT:
+                quit()
+            # Disparos del jugador
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    player.shoot()
+
+        # Eventos para el movimiento del jugador
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a] and player.x >= 0:
+            player.x -= player.speed
+        if keys[pygame.K_d] and player.x <= WIDTH-player.size:
+            player.x += player.speed
+        if keys[pygame.K_w] and player.y >= 0:
+            player.y -= player.speed
+        if keys[pygame.K_s] and player.y <= HEIGHT-player.size:
+            player.y += player.speed
+
+# Menu principal
+run = True
+while run:
+    screen.fill(BACKGROUND)
+    screen.blit(label_title, (WIDTH//2 - label_title.get_width()//2, HEIGHT//2))
     pygame.display.update()
 
-
-
-is_running = True
-while is_running:
-    clock.tick(FPS)
-    redraw_window()
-
-    # Si pierdes todas las vidas o destruyen la nave del jugador entonces pierde el juego
-    if player.lives <= 0 or player.health <= 0:
-        lost = True
-        lost_count += 1
-        if lost_count > FPS*3:
-            is_running = False
-        else:
-            continue
-
-    # Si eliminan todos los enemigos, sube de nivel y agregamos nuevos enemigos
-    if not enemies:
-        level += 1
-        wave_length += 5
-        war_sound.play()
-        for i in range(wave_length):
-            enemy = Enemy(randrange(0, WIDTH-SHIP_SIZE), randrange(-1500, -100))
-            enemies.append(enemy)
-
-    # Deteccion de eventos
     for event in pygame.event.get():
-        # Cerrar la ventana del juego
         if event.type == pygame.QUIT:
-            is_running = False
-        # Disparos del jugador
+            run = False
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                player.shoot()
-
-    # Eventos para el movimiento del jugador
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_a] and player.x >= 0:
-        player.x -= player.speed
-    if keys[pygame.K_d] and player.x <= WIDTH-player.size:
-        player.x += player.speed
-    if keys[pygame.K_w] and player.y >= 0:
-        player.y -= player.speed
-    if keys[pygame.K_s] and player.y <= HEIGHT-player.size:
-        player.y += player.speed
+            run_game()
 
 pygame.quit()
